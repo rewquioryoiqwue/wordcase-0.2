bb40b8239ffeb52bce484bc455a401a3758feee4
shuhei-cyf <shuhei.cyf@gmail.com>
2019-10-25 18:54:41 +0900

    Initial Commit

---
+++ wordcase 0.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordcase 0.2
+//
+//  Created by 新谷修平 on 2019/10/25.
+//  Copyright © 2019 Shuhei Shintani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
